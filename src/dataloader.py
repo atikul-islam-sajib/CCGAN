15c6889aa8be5c25d11cf15d9a9671a370bcb7ac
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-07-17 23:17:08 +0200

    dataset is updated

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,15 +2,21 @@ import zipfile
 
 from .utils import config, dump, load, CustomException
 
+
 class Loader:
-    def __init__(self, image_size: int = 128, channels: int = 3, batch_size: int = 8, split_size: float = 0.20):
+    def __init__(
+        self,
+        image_size: int = 128,
+        channels: int = 3,
+        batch_size: int = 8,
+        split_size: float = 0.20,
+    ):
         self.image_size = image_size
         self.channels = channels
         self.batch_size = batch_size
         self.split_size = split_size
-        
+
         self.CONFIG = config()
-        
+
     def unzip_folder(self):
         pass
-        
