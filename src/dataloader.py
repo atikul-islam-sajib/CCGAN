1d3077eca6561de0e23c1f97b8fcadd56f188c06
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-07-17 23:49:57 +0200

    code is updated in the dataloader

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,4 +1,9 @@
+import os
+import cv2
 import zipfile
+from PIL import Image
+from torch.utils.data import DataLoader
+from torchvision import transforms
 
 from utils import config, dump, load, validate_path, CustomException
 
@@ -20,6 +25,10 @@ class Loader:
 
         self.CONFIG = config()
 
+        self.independent: list = []
+        self.dependent: list = []
+        self.lr_image: list = []
+
     def unzip_folder(self):
         if validate_path(path=self.CONFIG["path"]["RAW_IMAGE_DATA_PATH"]):
             with zipfile.ZipFile(file=self.image_path, mode="r") as zip_file:
@@ -28,12 +37,78 @@ class Loader:
         else:
             raise CustomException("Raw data path cannot be found".capitalize())
 
+    def transforms(self, type="lr"):
+        if type == "hr":
+            return transforms.Compose(
+                [
+                    transforms.Resize((self.image_size, self.image_size)),
+                    transforms.CenterCrop((self.image_size, self.image_size)),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+                ]
+            )
+
+        elif type == "lr":
+            return transforms.Compose(
+                [
+                    transforms.Resize((self.image_size // 4, self.image_size // 4)),
+                    transforms.CenterCrop((self.image_size // 4, self.image_size // 4)),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+                ]
+            )
+
+    def feature_extractor(self):
+        self.directory = os.path.join(
+            self.CONFIG["path"]["RAW_IMAGE_DATA_PATH"], "dataset"
+        )
+        assert (
+            self.directory.split("/")[-1] == "dataset"
+        ), "Directory name should be dataset"
+
+        self.X = os.path.join(self.directory, "X")
+        self.y = os.path.join(self.directory, "y")
+
+        print(self.X, self.y)
+
+        assert (
+            self.X.split("/")[-1] == "X" and self.y.split("/")[-1] == "y"
+        ), "Directory name should be X and y"
+
+        for index, image in enumerate(os.listdir(self.X)):
+            if image in os.listdir(self.y):
+                self.imageX = os.path.join(self.X, image)
+                self.imageY = os.path.join(self.y, image)
+
+                self.imageX = cv2.imread(self.imageX)
+                self.imageY = cv2.imread(self.imageY)
+
+                self.imageX = cv2.cvtColor(self.imageX, cv2.COLOR_BGR2RGB)
+                self.imageY = cv2.cvtColor(self.imageY, cv2.COLOR_BGR2RGB)
+
+                self.imageX = Image.fromarray(self.imageX)
+                self.imageY = Image.fromarray(self.imageY)
+
+                self._imageX = self.transforms(type="hr")(self.imageX)
+                self._imageY = self.transforms(type="hr")(self.imageY)
+
+                self.lr_imageX = self.transforms(type="lr")(self.imageX)
+                self.lr_imageY = self.transforms(type="lr")(self.imageY)
+
+                self.independent.append(self._imageX)
+                self.dependent.append(self._imageY)
+                self.lr_image.append(self.lr_imageX)
+
+        assert len(self.independent) == len(self.dependent) == len(self.lr_image)
+
 
 if __name__ == "__main__":
     loader = Loader(image_path="data/raw/dataset.zip")
-    try:
-        loader.unzip_folder()
-    except CustomException as e:
-        print(e)
-    except Exception as e:
-        print(e)
+    # try:
+    #     loader.unzip_folder()
+    # except CustomException as e:
+    #     print(e)
+    # except Exception as e:
+    #     print(e)
+
+    loader.feature_extractor()
