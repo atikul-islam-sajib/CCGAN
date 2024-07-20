2952d208f5ff4dfccf451cfc2d7b53fb8adaa2b5
Atikul Islam Sajib <112280249+atikul-islam-sajib@users.noreply.github.com>
2024-07-21 00:31:22 +0200

    Update tester.py

--- src/tester.py
+++ src/tester.py
@@ -2,6 +2,7 @@ import os
 import sys
 import torch
 import traceback
+import matplotlib.pyplot as plt
 
 sys.path.append("./")
 
@@ -30,6 +31,25 @@ class Tester:
 
         return self.netG
 
+    def load_dataloader(self):
+        if self.dataloader == "valid":
+            processed_path = config()["path"]["PROCESSED_IMAGE_DATA_PATH"]
+
+            if validate_path(path=processed_path):
+                return load(
+                    filename=os.path.join(processed_path, "valid_dataloader.pkl")
+                )
+            else:
+                raise FileNotFoundError("Valid dataloader not found".capitalize())
+
+        else:
+            if validate_path(path=processed_path):
+                return load(
+                    filename=os.path.join(processed_path, "valid_dataloader.pkl")
+                )
+            else:
+                raise FileNotFoundError("Train dataloader not found".capitalize())
+
     def select_model(self):
         if self.model == "best":
             bestmodel = config()["path"]["BEST_MODEL"]
@@ -52,11 +72,65 @@ class Tester:
                     "Model not found, that is defined by user".capitalize()
                 )
 
+    def plot_images(self, model: Generator):
+        try:
+            dataloader = self.load_dataloader()
+        except FileNotFoundError as e:
+            print(
+                "An error occured while loading the dataloader {}".capitalize().format(
+                    e
+                )
+            )
+        except Exception as e:
+            print(
+                "An error occured while loading the dataloader {}".capitalize().format(
+                    e
+                )
+            )
+        else:
+            X, y, lr_image = next(iter(dataloader))
+
+            X = X.to(self.device)
+            y = y.to(self.device)
+            lr_image = lr_image.to(self.device)
+
+            generated_image = model(X, lr_image)
+
+            number_of_rows = X.size(0) // 2
+            number_of_coumns = X.size(0) // number_of_rows
+
+        plt.figure(figsize=(10, 10))
+
+        for index, image in enumerate(X):
+            realimage = image.permute(2, 1, 0).detach().cpu().numpy()
+            generated = generated_image[index].permute(2, 1, 0).cpu().detach().numpy()
+
+            realimage = (realimage - realimage.min()) / (
+                realimage.max() - realimage.min()
+            )
+            generated = (generated - generated.min()) / (
+                generated.max() - generated.min()
+            )
+
+            plt.subplot(2 * number_of_rows, 2 * number_of_coumns, 2 * index + 1)
+            plt.imshow(realimage)
+            plt.title("Real".capitalize())
+            plt.axis("off")
+
+            plt.subplot(2 * number_of_rows, 2 * number_of_coumns, 2 * index + 2)
+            plt.imshow(generated)
+            plt.title("Generated".capitalize())
+            plt.axis("off")
+
+        plt.show()
+
     def test(self):
         netG = self.initialize_netG()
         try:
             netG.load_state_dict(self.select_model())
-            print(netG)
+
+            self.plot_images(model=netG)
+
         except FileNotFoundError as e:
             print("An error occurred: ", e)
             traceback.print_exc()
@@ -66,5 +140,5 @@ class Tester:
 
 
 if __name__ == "__main__":
-    tester = Tester(model="./checkpoints/train_models/netG2.pth", device="mps")
+    tester = Tester(model="best", device="mps")
     tester.test()
